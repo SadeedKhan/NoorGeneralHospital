cb4dfed9b8df5a8d56c9228dbcac0ba7319f4a82
Dev <sadeed.cloudtek@gmail.com>
2021-04-25 02:55:47 +0500

    Updating Migration

---
+++ NoorGeneralHospital/NoorGeneralHospital/Services/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace NoorGeneralHospital.Services
+{
+    public class Email
+    {
+        public static string Mail(string ToEmail, string subject, string Body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(ToEmail);
+                // mail.To.Add("Another Email ID where you wanna send same email");
+                mail.From = new MailAddress("aalerts7@gmail.com");
+                mail.Subject = subject;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment(ConfigurationManager.ConnectionStrings["DBbackup"].ConnectionString));
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com"; //Or Your SMTP Server Address
+                smtp.Credentials = new System.Net.NetworkCredential
+                     ("aalerts7@gmail.com", "!23#Alerts");
+                //Or your Smtp Email ID and Password
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return "Email has been successfuly sent";
+            }
+            catch
+            {
+                return "Connection error! Please check the connection and try again";
+            }
+        }
+    }
+}
---
+++ NoorGeneralHospital/NoorGeneralHospital/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(NoorGeneralHospital.App_Start.Startup))]
+
+namespace NoorGeneralHospital.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
