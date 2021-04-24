ec34729a4702657ee5fe006f2ae4c7b291df0ffe
Dev <sadeed.cloudtek@gmail.com>
2021-04-25 03:12:47 +0500

    Updating Register Error

--- NoorGeneralHospital/NoorGeneralHospital/Areas/Admin/Controllers/AccountController.cs
+++ NoorGeneralHospital/NoorGeneralHospital/Areas/Admin/Controllers/AccountController.cs
@@ -161,7 +161,7 @@ namespace NoorGeneralHospital.Areas.Admin.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                    //return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Dashboard");
                 }
                 AddErrors(result);
             }
