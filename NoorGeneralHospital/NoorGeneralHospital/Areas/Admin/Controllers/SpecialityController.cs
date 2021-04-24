ed27beeb267f51f5a8a2cc0ed963549bdba8714f
Dev <sadeed.cloudtek@gmail.com>
2021-04-24 17:00:44 +0500

    Updating Style and Login Page design

---
+++ NoorGeneralHospital/NoorGeneralHospital/Areas/Admin/Controllers/SpecialityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NoorGeneralHospital.Areas.Admin.Controllers
+{
+    public class SpecialityController : Controller
+    {
+        // GET: Admin/Speciality
+        public ActionResult Index()
+        {
+            return View();
+        }
+         public ActionResult _AddSpeciality()
+        {
+            return View();
+        }
+    }
+}
