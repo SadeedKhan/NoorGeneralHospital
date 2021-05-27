fb1dcfa9ede91dc26de1d0eab9d6c1c11b650cd0
Hassan Hasni <hasni_1965@outlook.com>
2021-05-27 19:05:45 +0500

    Updating Loader

--- NoorGeneralHospital/NoorGeneralHospital/Content/User/assets/js/my-preloader.js
+++ NoorGeneralHospital/NoorGeneralHospital/Content/User/assets/js/my-preloader.js
@@ -1,4 +1,6 @@
-window.onload = function() {
-    document.querySelector('.preloader').style.display = "none"; 
-    document.querySelector('.page').style.opacity = "1"; 
+window.onload = function () {
+     
+            document.querySelector('.preloader').style.display = "none";
+            document.querySelector('.page').style.opacity = "1"; 
+          
 }
